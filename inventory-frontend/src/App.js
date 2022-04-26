6f14b9731c84eb398254c97f40b874dedbe93f20
pranavdev540 <pranav.dev540@gmail.com>
2022-04-27 01:50:32 +0530

    new initial commits

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import StockEventTable from "./components/StockEventTable";
 
-function App() {
+const fetchProduct = [
+  { id: 1, name: "Iphone X", price: 1000 },
+  { id: 2, name: "Iphone XS", price: 2000 },
+  { id: 3, name: "Iphone XR", price: 3000 },
+  { id: 4, name: "Iphone XS Max", price: 4000 },
+];
+
+const fetchstockEvent = [
+  { id: 1, type: "add", qty: 100, product: fetchProduct[0] },
+  { id: 2, type: "remove", qty: 143, product: fetchProduct[3] },
+  { id: 3, type: "add", qty: 100, product: fetchProduct[1] },
+  { id: 4, type: "remove", qty: 143, product: fetchProduct[2] },
+  { id: 5, type: "add", qty: 100, product: fetchProduct[0] },
+];
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Stocks </h1>
+      <StockEventTable
+        title="new-alex"
+        product={fetchProduct}
+        stockEvent={fetchstockEvent}
+      />
     </div>
   );
-}
+};
 
 export default App;
